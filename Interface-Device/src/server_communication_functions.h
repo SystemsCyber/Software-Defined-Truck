fd590797226d036c3bd5574b417b1c93259576f0
Ben Ettlinger <39038496+benettlinger@users.noreply.github.com>
2021-06-01 18:30:46 -0600

    Initial Commit of UDP Communication Header
    Basic list of header functions with some code added based upon Arduino Test File

---
+++ Interface-Device/src/server_communication_functions.h
@@ -0,0 +1,46 @@
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
+char ReplyBuffer[] = "acknowledged";        // a string to send back
+
+void init_UDP_connection(byte mac[6], unsigned int localPort, IPAddress remoteip){
+if (Ethernet.begin(mac) == 0) {
+
+    Serial.println("Failed to configure Ethernet using DHCP");
+
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+
+    } else if (Ethernet.linkStatus() == LinkOFF) {
+
+      Serial.println("Ethernet cable is not connected.");
+
+    }
+  }
+
+  Serial.print("Obtained IP: "); Serial.println(Ethernet.localIP());
+
+  
+  // start UDP
+  Udp.begin(localPort);
+ 
+}
+
+void UDP_send_CAN() {
+    Serial.println("Sending CAN Frame over UDP: Unfinished")
+
+}
+
+void UDP_receive_CAN() {
+    Serial.println("Receiving CAN Frame over UDP: Unfinished")
+}
+
+struct UDP_CAN_Package{
+    int index;
+    char arb_ID[4];
+    int dlc;
+    int data[8];
+}
