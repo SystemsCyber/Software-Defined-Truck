ba701c8f50cebb9329a292748067551afd3a74f7
Jepson2k <55201008+Jepson2k@users.noreply.github.com>
2021-09-13 17:49:34 -0600

    Moved Device static methods to the device class I accidentally deleted earlier.

--- Carla/PythonAPI/AMP/HelperMethods.py
+++ Carla/PythonAPI/AMP/HelperMethods.py
@@ -1,12 +1,9 @@
 import os
 import json
 import jsonschema
-import selectors
 import logging
 import copy
 from typing import Dict, Tuple, List
-
-SEL = selectors.SelectorKey
 
 class Schema:
     @staticmethod
@@ -26,52 +23,6 @@ class Schema:
                 if name == "Schemas":
                     return os.path.join(root, name)
         return os.path.join(base_dir, "Schemas")
-
-class Device:
-    @staticmethod
-    def is_registered(key: SEL) -> bool:
-        if hasattr(key.data, "MAC") and key.data.MAC != "unknown":
-            return True
-        return False
-
-    @staticmethod
-    def is_not_listening_socket(key: SEL) -> bool:
-        return hasattr(key.data, "MAC")
-
-    @staticmethod
-    def is_client(key: SEL) -> bool:
-        if Device.is_registered():
-            return hasattr(key.data, "type") and key.data.type == "CLIENT"
-        else:
-            return False
-    
-    @staticmethod
-    def is_SSS3(key: SEL) -> bool:
-        if hasattr(key.data, "MAC") and key.data.MAC != "unknown":
-            return hasattr(key.data, "type") and key.data.type == "SSS3"
-        else:
-            return False
-
-    @staticmethod
-    def is_available(key: SEL) -> bool:
-        if Device.is_not_listening_socket(key):
-            return Device.is_SSS3(key) and Device.is_free(key)
-
-    @staticmethod
-    def is_free(key: SEL) -> bool:
-        empty_carla_addr = key.data.session.CARLA_MCAST == None
-        empty_can_addr = key.data.session.CAN_MCAST == None
-        return empty_carla_addr and empty_can_addr
-
-    @staticmethod
-    def get_available_ECUs(sel: selectors.DefaultSelector) -> Dict:
-        available = {}
-        sel_map = sel.get_map()
-        for fd in sel_map:
-            key = sel_map[fd]
-            if Device.is_available(key):
-                available[str(fd)] = key.data.ECUs
-        return available
 
 
 # COPIED FROM: https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output?page=1&tab=votes#tab-top
