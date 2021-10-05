f7dc54e030b8cac7b452253cbbb73cad63822d61
Jepson2k <55201008+Jepson2k@users.noreply.github.com>
2021-10-05 14:35:43 -0600

    Imports simplification.

--- Carla/PythonAPI/AMP/Broker/broker.py
+++ Carla/PythonAPI/AMP/Broker/broker.py
@@ -1,14 +1,11 @@
 from http import HTTPStatus
+from argparse import ArgumentParser
 import queue
-import socket
-import os
-import time
-import sys
-import selectors
+from socket import *
+from time import time, sleep
+from selectors import *
 from ipaddress import ip_network
 from http.server import BaseHTTPRequestHandler
-from types import SimpleNamespace
-from typing import Tuple, Dict, Union, List, Optional
 from types import SimpleNamespace
 from io import BytesIO
 from Device import Device
@@ -20,7 +17,6 @@ from HelperMethods import ColoredConsoleHandler, LogFolder
 import http.client
 
 """TODO Notes:
-    - We need to set the keep alive function on the teensy.
     - When recving or sending timeout should be calculated dynamically.
         - From:
           https://www.geeksforgeeks.org/algorithm-for-dynamic-time-out-timer-calculation/
@@ -28,19 +24,18 @@ import http.client
               calculate timeout and when to retransmit.
 """
 
-# Type Aliases
-SOCKTYPE = socket.socket
-SEL = selectors.SelectorKey
-
 class Broker(BaseHTTPRequestHandler):
 
     # ==================== Initialization ====================
 
-    def __init__(self) -> None:
+    def __init__(self, _keepalive_interval = 300, _localhost = False) -> None:
+        self.client_address = None
+        self.keepalive_interval = _keepalive_interval
+        self.localhost = _localhost
         self.__setup_logging()
         self.log_message("Broker Initializing.")
         self.protocol_version = "HTTP/1.1"
-        self.sel = selectors.DefaultSelector()
+        self.sel = DefaultSelector()
         self.multicast_IPs = []
         for ip in ip_network('239.255.0.0/16'):
             self.multicast_IPs.append({
@@ -75,14 +70,14 @@ class Broker(BaseHTTPRequestHandler):
 
     def log_error(self, format, *args):
         address_string = "SERVER"
-        if hasattr(self, "client_address"):
+        if self.client_address:
             address_string = self.client_address[0]
         logging.error("%s - - %s\n" %
                          (address_string, format%args))
 
     def log_message(self, format, *args):
         address_string = "SERVER"
-        if hasattr(self, "client_address"):
+        if self.client_address:
             address_string = self.client_address[0]
         logging.info("%s - - %s\n" %
                          (address_string, format%args))
@@ -112,15 +107,15 @@ class Broker(BaseHTTPRequestHandler):
     # ==================== Main Server Functions ====================
 
     def listen(self):
-        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        device_address = socket.gethostbyname_ex(socket.gethostname())[2][3]
+        lsock = socket(AF_INET, SOCK_STREAM)
+        lsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        device_address = gethostbyname_ex(gethostname())[2][3]
         lsock.bind((device_address, 80))
         # lsock.bind(("127.0.0.1", 80))
         lsock.listen()
         lsock.setblocking(False)
         data = SimpleNamespace(callback=self.__accept)
-        self.sel.register(lsock, selectors.EVENT_READ, data=data)
+        self.sel.register(lsock, EVENT_READ, data=data)
         self.log_message(f'Listening on: {device_address}:80')
         self.__handle_connection()
 
@@ -137,35 +132,37 @@ class Broker(BaseHTTPRequestHandler):
                     self.__handle_mask_events(key, mask)
                 self.__prune_connections()
 
-    def __handle_mask_events(self, key: SEL, mask):
-        if mask == selectors.EVENT_READ:
+    def __handle_mask_events(self, key: SelectorKey, mask):
+        if mask == EVENT_READ:
             if key.data.callback == self.__accept:
                 callback = key.data.callback
                 callback(key)
-            elif not key.data.rate_limit:
-                time.sleep(0.001)  # Small hack to let the rest of the message
+            elif not key.data.rate_limit(self.log_error):
+                sleep(0.001)  # Small hack to let the rest of the message
                                     # make it in to the socket before reading it.
                 self.__call_callback(key)
         else:
             self.__call_callback(key)
 
-    def __call_callback(self, key: SEL):
-        with key.data.addr as self.client_address:
-            callback = key.data.callback
-            callback(key)
+    def __call_callback(self, key: SelectorKey):
+        self.client_address = key.data.addr
+        callback = key.data.callback
+        callback(key)
+        self.client_address = None
 
     def __prune_connections(self) -> None:
         """Loose connections are accepted connections from devices that don't
-        register within 5 seconds of connecting."""
-        current_time = time.time()
-        for fd, key in self.sel.get_map():
-            try:
-                if key.data.is_loose(current_time, self.log_error):
-                    self.__shutdown_connection(key)
-            except AttributeError:
-                continue
+        register within 30 seconds of connecting."""
+        current_time = time()
+        sel_map = self.sel.get_map()
+        for fd in sel_map:
+            key = sel_map[fd]
+            not_lsock = hasattr(key.data, "MAC")
+            if not_lsock and key.data.is_loose(current_time, self.log_error):
+                key.data.close_connection = True
+                self.sel.modify(key.fileobj, EVENT_WRITE, key.data)
 
-    def __accept(self, key: SEL) -> None:
+    def __accept(self, key: SelectorKey) -> None:
         """Takes a new connection from the listening socket and assigns it its
         own socket. Then registers it with the selectors using the READ mask.
         """
@@ -173,59 +170,62 @@ class Broker(BaseHTTPRequestHandler):
         conn.setblocking(False)
         if addr[0] in self.blacklist_ips:
             self.log_error(f'Blacklisted IP address: {addr[0]} tried to connect.')
-            conn.shutdown(socket.SHUT_RDWR)
+            conn.shutdown(SHUT_RDWR)
             conn.close()
         else:
             self.log_message(f'New connection from: {addr[0]}:{str(addr[1])}')
-            self.__set_keepalive(conn, 300000, 300000)
+            self.__set_keepalive(conn)
             data = Device(self.__read, self.__write, addr)
-            self.sel.register(conn, selectors.EVENT_READ, data=data)
+            self.sel.register(conn, EVENT_READ, data=data)
 
-    def __set_keepalive(self, conn: SOCKTYPE, idle_sec: int, interval_sec: int) -> None:
-        if os.name == 'nt':
-            conn.ioctl(socket.SIO_KEEPALIVE_VALS, (1, idle_sec, interval_sec))
-        else:
-            conn.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-            conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, idle_sec)
-            conn.setsockopt(socket.IPPROTO_TCP,
-                            socket.TCP_KEEPINTVL, interval_sec)
-            conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 10)
+    def __set_keepalive(self, conn: SocketType) -> None:
+            conn.setsockopt(SOL_SOCKET, SO_KEEPALIVE, 1)
+            conn.setsockopt(IPPROTO_TCP, TCP_KEEPIDLE, self.keepalive_interval)
+            conn.setsockopt(IPPROTO_TCP, TCP_KEEPINTVL, self.keepalive_interval)
+            conn.setsockopt(IPPROTO_TCP, TCP_KEEPCNT, 3)
 
-    def __read(self, key: SEL):
+    def __read(self, key: SelectorKey):
         if key.data.expecting_response:
             self.__handle_response(key)
         else:
             try:
-                message = self._key.fileobj.recv(4096)
-                self.__handle_request(key, message)
+                message = key.fileobj.recv(4096)
             except OSError as ose:
+                self.log_error(f'{ose}')
                 key.data.close_connection = True
                 self.__shutdown_connection(key)
-                self.log_error(ose)
-
-    def __handle_request(self, key: SEL, message: bytes) -> None:
+            else:
+                self.__handle_request(key, message)
+    
+    def __handle_request(self, key: SelectorKey, message: bytes) -> None:
         with BytesIO() as self.wfile, BytesIO(message) as self.rfile:
+            self._key = key
             self.handle_one_request()
-            if len(self.wfile) > 0:
-                self.end_headers()
-                self.wfile.seek(0)
-                key.data.outgoing_messages.put(self.wfile.read())
+            self.end_headers()
+            self.wfile.seek(0)
+            outgoing = self.wfile.read()
+            if len(outgoing) > 0:
+                key.data.outgoing_messages.put(outgoing)
                 key.data.callback = self.__write
                 key.data.close_connection = self.close_connection
-                self.sel.modify(key.fileobj, selectors.EVENT_WRITE, key.data)
+                self.sel.modify(key.fileobj, EVENT_WRITE, key.data)
             else:
                 logging.info(f'{key.data.addr[0]} - - Closed the connection.')
                 self.__shutdown_connection(key)
 
-    def __handle_response(self, key: SEL):
+    def __handle_response(self, key: SelectorKey):
         key.data.response = http.client.HTTPResponse(key.fileobj)
         try:
             key.data.response.begin()
             key.data.expecting_response = False
         except http.client.HTTPException as he:
-            self.log_error(he)
+            self.log_error(f'{he}')
             key.data.close_connection = True
             self.__shutdown_connection(key)
+        except OSError as ose:
+                self.log_error(f'{ose}')
+                key.data.close_connection = True
+                self.__shutdown_connection(key)
 
     def do_GET(self):
         self.__method_poxy()
@@ -256,7 +256,7 @@ class Broker(BaseHTTPRequestHandler):
         try:
             device_list = getattr(self, list_name)
         except AttributeError as ae:
-            logging.debug(ae)
+            logging.debug(f'{ae}')
             self.log_error("Requested a non-existent device type.")
             self.send_error(HTTPStatus.NOT_FOUND)
             return
@@ -268,7 +268,7 @@ class Broker(BaseHTTPRequestHandler):
                 self._key.data.close_connection = True
             self.send_response(response)
         except AttributeError as ae:
-            logging.debug(ae)
+            logging.debug(f'{ae}')
             self.log_error("Requested a method that is not implemented.")
             self.send_error(HTTPStatus.NOT_IMPLEMENTED)
 
@@ -283,30 +283,50 @@ class Broker(BaseHTTPRequestHandler):
             method_name = 'do_' + self.command.upper() + verb_name 
         return list_name, method_name
 
-    def __write(self, key: SEL):
+    def __write(self, key: SelectorKey):
         try:
             while True:
                 message = key.data.outgoing_messages.get_nowait()
                 key.fileobj.sendall(message)
         except queue.Empty:
             key.data.callback = self.__read
-            self.sel.modify(key.fileobj, selectors.EVENT_READ, key.data)
+            self.sel.modify(key.fileobj, EVENT_READ, key.data)
         except OSError as ose:
-            self.log_error(ose)
+            self.log_error(f'{ose}')
             key.data.close_connection = True
         finally:
             if key.data.close_connection:
                 self.log_message(f'Closed the connection.')
                 self.__shutdown_connection(key)
 
-    def __shutdown_connection(self, key: SEL):
+    def __shutdown_connection(self, key: SelectorKey):
         logging.info(f'{key.data.addr[0]} - - Closing connection.')
+        if getattr(key.data, "type"):
+            list_name = getattr(self, key.data.type + "s")
+            del_method = getattr(list_name, "do_DELETE_register")
+            del_method(key)
         self.sel.unregister(key.fileobj)
-        key.fileobj.shutdown(socket.SHUT_RDWR)
+        key.fileobj.shutdown(SHUT_RDWR)
         key.fileobj.close()
 
 def main():
-    broker = Broker()
+    argparser = ArgumentParser(
+        description="DARPA AMP CARLA Broker"
+        )
+    argparser.add_argument(
+        '-k', '--keep_alive_interval',
+        default=300,
+        type=int,
+        help='Keep alive probe interval for sockets.'
+        )
+    argparser.add_argument(
+        '-l', '--localhost',
+        action='store_true',
+        default=False,
+        help='Use localhost interface for listening socket.'
+    )
+    args = argparser.parse_args()
+    broker = Broker(args.keep_alive_interval, args.localhost)
     broker.listen()
 
 if __name__ == "__main__":
