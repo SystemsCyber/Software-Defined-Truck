e0fb6a6ad512b7af659ca4acd6b85b74807f5660
Jepson2k <55201008+Jepson2k@users.noreply.github.com>
2021-10-31 23:28:21 -0600

    wrote the network stats file

--- Src/SSSF/src/NetworkStats/NetworkStats.cpp
+++ Src/SSSF/src/NetworkStats/NetworkStats.cpp
@@ -1,37 +1,57 @@
 #include <Arduino.h>
+#include <CANNode/CANNode.h>
 #include <NetworkStats/NetworkStats.h>
 #include <FlexCAN_T4.h>
 
-void NetworkStats::update(CAN_message_t *msg)
+NetworkStats::NetworkStats(uint16_t _id, uint16_t *_members, size_t _size):
+    id(_id), members(_members), size(_size),
+    Basics(new HealthBasics [_size]),
+    HealthReport(new NodeReport [_size])
+{}
+
+NetworkStats::~NetworkStats()
+{
+    delete &HealthReport;
+    delete &Basics;
+}
+
+void NetworkStats::update(uint16_t _id, int packetSize, uint32_t timestamp, uint32_t sequenceNumber)
 {
     for (size_t i = 0; i < size; i++)
     {
-        
+        if (members[i] == _id)
+        {
+            struct NodeReport &node = HealthReport[i];
+            struct HealthBasics &basics = Basics[i];
+            float now = float(millis());
+            basics.count++;
+            calculate(node.latency, basics.count, now - timestamp);
+            calculate(node.jitter, basics.count, node.latency.variance);
+            node.packetLoss = (sequenceNumber - basics.count) / sequenceNumber;
+            float ellapsedSeconds = (now - basics.lastMessageTime) / 1000;
+            calculate(node.throughput, basics.count, (packetSize * 8) / ellapsedSeconds);
+            basics.lastMessageTime = now;
+        }
     }
 }
 
 void NetworkStats::reset()
 {
-    delta = 0;
-    delta2 = 0;
-
     for (size_t i = 0; i < size; i++)
     {
-        memset(&latency[i], 0, sizeof(struct AggregateFunctions));
-        memset(&jitter[i], 0, sizeof(struct AggregateFunctions));
-        memset(&packetLoss[i], 0, sizeof(struct AggregateFunctions));
-        memset(&throughput[i], 0, sizeof(struct AggregateFunctions));
+        Basics[i].count = 0;
+        Basics[i].lastMessageTime = millis();
+        HealthReport[i] = {0};
     }
 }
 
-void NetworkStats::calculate(struct AggregateFunctions *edge, uint32_t n)
+void NetworkStats::calculate(struct HealthCore &edge, uint32_t &count, float n)
 {// From: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm
-    edge->min = (n < edge->min) ? n : edge->min;
-    edge->max = (n > edge->max) ? n : edge->max;
-    edge->count++;
-    delta = n - edge->mean;
-    edge->mean += delta / edge->count;
-    delta2 = n - edge->mean;
-    edge->M2 += delta * delta2;
-    edge->variance = edge->M2 / edge->count;
+    edge.min = min(edge.min, n);
+    edge.max = max(edge.max, n);
+    delta = n - edge.mean;
+    edge.mean += delta / count;
+    delta2 = n - edge.mean;
+    edge.M2 += delta * delta2;
+    edge.variance = edge.M2 / count;
 }
--- Src/SSSF/src/NetworkStats/NetworkStats.h
+++ Src/SSSF/src/NetworkStats/NetworkStats.h
@@ -2,53 +2,54 @@
 #define network_stats_h_
 
 #include <Arduino.h>
+#include <CANNode/CANNode.h>
+#include <limits>
 #include <FlexCAN_T4.h>
 
 class NetworkStats
 {
 private:
     uint16_t id;
-    uint16_t *ids;
-    size_t size = 0;
+    uint16_t *members;
 
-    uint32_t delta = 0;
-    uint32_t delta2 = 0;
-
-    struct AggregateFunctions
-    {
-        uint32_t count = 0;
-        uint32_t min = (2^32) - 1;
-        double mean = 0.0;
-        uint32_t max = 0;
-        uint32_t variance = 0;
-        uint32_t M2 = 0;
-    };
+    float delta = 0;
+    float delta2 = 0;
 
 public:
-    struct AggregateFunctions *latency;
-    struct AggregateFunctions *jitter;
-    struct AggregateFunctions *packetLoss;
-    struct AggregateFunctions *throughput;
-
-    NetworkStats(uint16_t _id, uint16_t *_ids, size_t _size) :
-        id(_id), ids(_ids), size(_size),
-        latency(new struct AggregateFunctions [_size]),
-        jitter(new struct AggregateFunctions [_size]),
-        packetLoss(new struct AggregateFunctions [_size]),
-        throughput(new struct AggregateFunctions [_size])
-        {};
-    ~NetworkStats()
+    struct HealthBasics
     {
-        delete &latency;
-        delete &jitter;
-        delete &packetLoss;
-        delete &throughput;
+        uint32_t count;
+        uint32_t lastMessageTime = millis();
     };
-    void update(CAN_message_t *msg);
+
+    struct HealthCore
+    {
+        float min = std::numeric_limits<float>::max();
+        float max = -std::numeric_limits<float>::max();
+        float mean = 0.0;
+        float variance = 0;
+        float M2 = 0;
+    };
+
+    struct NodeReport
+    {
+        float packetLoss;
+        struct HealthCore latency;
+        struct HealthCore jitter;
+        struct HealthCore throughput;
+    };
+
+    size_t size = 0;
+    struct HealthBasics *Basics;
+    struct NodeReport *HealthReport;
+
+    NetworkStats(uint16_t _id, uint16_t *_members, size_t _size);
+    ~NetworkStats();
+    void update(uint16_t _id, int packetSize, uint32_t timestamp, uint32_t sequenceNumber);
     void reset();
 
 private:
-    void calculate(struct AggregateFunctions *m, uint32_t n);
+    void calculate(struct HealthCore &edge, uint32_t &count, float n);
 };
 
 #endif /* network_stats_h_ */
