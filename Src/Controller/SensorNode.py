7262783aedc5f1b02afd079f2ac311d897387193
Jepson2k <55201008+Jepson2k@users.noreply.github.com>
2021-11-01 18:03:49 -0600

    adjusted sensornode to work with the new sensornode struct

--- Src/Controller/SensorNode.py
+++ Src/Controller/SensorNode.py
@@ -1,37 +1,24 @@
-import struct
-from collections import namedtuple
-from typing import Tuple
 from CANNode import CANNode
-from Frame import CAN_Message_T
+from ctypes import *
+
+
+class WSenseBlock(Structure):
+    _fields_ = [
+        ("num_signals", c_uint8),
+        ("signals", c_float * 19)
+    ]
+
+    def __repr__(self) -> str:
+        s = f'num_signals: {self.num_signals} signals:\n'
+        for i in range(self.num_signals):
+            s += f'{self.signals[i]}\n'
+        return s
 
 class SensorNode(CANNode):
-    def __init__(self, _max_retransmissions: int) -> None:
-        self.frame_num = 0
-        self.last_frame_num = 0
-        self.frame_rate = 0
-        self.retransmission_timeout = 0
-        self.max_retransmissions = _max_retransmissions
+    def __init__(self) -> None:
         super(SensorNode, self).__init__()
 
-    def write(self, message) -> None:
-        if isinstance(message, CAN_Message_T):
-            return super().write(message)
-        else:
-            if self.frame_num == 4294967296:
-                self.frame_num = 0
-            else:
-                self.frame_num += 1
-            self.last_frame_num = self.frame_num
-            self.last_frame = message
-            packed_message = struct.pack("Ifff???B",
-                           self.frame_num,              # Frame number
-                           message.throttle,            # Throttle
-                           message.steer,               # Steering
-                           message.brake,               # Braking
-                           message.hand_brake,          # Hand Brake
-                           message.reverse,             # Reverse
-                           message.manual_gear_shift,   # Manual
-                           message.gear)                # Gear
-            return super().write(packed_message)
+    def packSignals(self, *sensors: float) -> WSenseBlock:
+        return WSenseBlock(len(sensors), sensors)
 
     
